5695e99336154464f27e4475eab27c8b8ff40bae
adrk17 <adrian.krupa17@gmail.com>
2024-10-19 15:13:56 +0200

    Initial project configuration

---
+++ main_api/RpsApi/Models/Middlewares/ExceptionHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace RpsApi.Models.Middlewares;
+using System.Net;
+
+public class ExceptionHandler
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandler> _logger;
+    public record ExceptionResponse(HttpStatusCode StatusCode, string Description);
+    
+    public ExceptionHandler(RequestDelegate next, ILogger<ExceptionHandler> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        ExceptionResponse response = exception switch
+        {
+            _ => new ExceptionResponse(HttpStatusCode.InternalServerError, exception.Message)
+        };
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)response.StatusCode;
+        await context.Response.WriteAsJsonAsync(response);
+    }
+}
--- main_api/RpsApi/Program.cs
+++ main_api/RpsApi/Program.cs
@@ -1,13 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using RpsApi.Database;
+using RpsApi.Models.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Database connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("RpsDatabaseConnection")));
 
 // Add services to the container.
 
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Cors config
+const string allowedOriginsPolicy = "_allowedOriginsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowedOriginsPolicy,
+        policy =>
+        {
+            policy.AllowAnyOrigin() // TODO-maybe: Change to AllowSpecificOrigins
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandler>();
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
