9a2d5a9122676356dc8af0c83cd52aacaaedb50c
adrk17 <adrian.krupa17@gmail.com>
2024-11-18 22:07:59 +0100

    Transformations fix

--- aimodel_api/aimodel/aimodels.py
+++ aimodel_api/aimodel/aimodels.py
@@ -40,14 +40,29 @@ class ModelManager:
             cls.loaded_models[model.id] = LoadedModel(keras_model, transforms)
         print(f"{len(cls.loaded_models)} models loaded into memory.")
 
-    def _apply_transforms(self, data):
-        pass
+    def _apply_transforms(self, image: Image.Image, transforms: list[TransformationType]):
+        for transform_id in transforms:
+            transform = Transformations.get_transform_by_id(transform_id)
+            if not transform:
+                raise ValueError(f"Transformation {transform_id} not found.")
+            image = transform.apply(image)
+            if image is None:
+                return None
+        return image
 
-    def _make_prediction(self, model: tf.keras.models.Model, data):
-        pass
 
-    def predict(self, model_id: int, data) -> tf.Tensor:
-        model = self.loaded_models.get(model_id)
-        if not model:
-            raise Exception(f"Model {model_id} not found.")
-        return self._make_prediction(model, data)
+
+    def predict(self, model_id: int, image: Image.Image) -> tf.Tensor:
+        model_data = self.loaded_models.get(model_id)
+        if not model_data:
+            raise Exception(f"Model {model_id} not loaded.")
+        
+        image = self._apply_transforms(image, model_data.transforms)
+        if image is None:
+            return None
+
+        if isinstance(image, np.ndarray):
+            if len(image.shape) == 3:  
+                image = np.expand_dims(image, axis=0)
+        
+        return model_data.model.predict(image)
--- aimodel_api/aimodel/transformations.py
+++ aimodel_api/aimodel/transformations.py
@@ -2,11 +2,14 @@ from PIL import Image
 import numpy as np
 import cv2
 import mediapipe as mp
+import tensorflow as tf
+from enum import Enum
 
 class Transformation:
     def apply(self, image):
         raise NotImplementedError("Each transformation must implement the 'apply' method.")
 
+# Rotates the image by the specified angle. image must be a PIL Image object
 class RotateImage(Transformation):
     def __init__(self, angle=90):
         self.angle = angle
@@ -14,13 +17,41 @@ class RotateImage(Transformation):
     def apply(self, image):
         if image.height > image.width:
             image.rotate(90, expand=True)
+        return image
 
+# Converts the image to grayscale using the L mode. image must be a PIL Image object
 class GrayscaleImage(Transformation):
     def apply(self, image):
         return image.convert('L')
 
+# Resizes the image to the target size using the Lanczos resampling method. image must be a PIL Image object
+class ResizeImage(Transformation):
+    def __init__(self, target_size=(224, 224)):
+        self.target_size = target_size
+
+    def apply(self, image):
+        return image.resize(self.target_size, Image.Resampling.LANCZOS)
+    
+# Normalizes the image array by dividing it by 255.0 to scale the pixel values to the range [0, 1], adds batch dimension
+class NormalizeImage(Transformation):
+    def apply(self, image):
+        if isinstance(image, Image.Image):
+            image_array = np.array(image)
+        else:
+            image_array = image
+        image_array = np.array(image)
+        image_array = np.expand_dims(image_array, axis=0)
+        image_array /= 255.0
+        return image_array
+    
+# Adds a channel to the image array to make it compatible with grayscale models that expect a channel dimension (e.g. (224, 224, 1))
+class AddChannel(Transformation):
+    def apply(self, image):
+        return np.expand_dims(image, axis=-1)
+
 mp_hands = mp.solutions.hands
 
+# Detects hands in the image using the MediaPipe Hands model. Returns a mask with the detected hand filled in white, None if no hand was detected. image must be a PIL Image object
 class HandDetection(Transformation):
     def __init__(self):
         self.hands = mp_hands.Hands(static_image_mode=True, max_num_hands=1, min_detection_confidence=0.5)
@@ -48,3 +79,33 @@ class HandDetection(Transformation):
 #         image_array = np.expand_dims(image_array, axis=0) 
 #         prediction = self.model.predict(image_array)
 #         return prediction[0]  
+
+
+# grayscale: conv grayscae, resize, normalize
+# mp_hands: resize, mp_hands, normalize
+# u_net: resize, u_net, normalize
+
+
+class TransformationType(Enum):
+    GRAYSCALE = 1
+    MP_HANDS = 2
+    U_NET = 3
+    RESIZE = 4
+    NORMALIZE = 5
+    ADD_CHANNEL = 6
+    ROTATE = 7
+    
+
+class Transformations:
+    @staticmethod
+    def get_transform_by_id(transform_id):
+        transformations = {
+            TransformationType.GRAYSCALE: GrayscaleImage(),
+            TransformationType.MP_HANDS: HandDetection(),
+            TransformationType.U_NET: UnetSegmentation(),
+            TransformationType.RESIZE: ResizeImage(),
+            TransformationType.NORMALIZE: NormalizeImage(),
+            TransformationType.ADD_CHANNEL: AddChannel(),
+            TransformationType.ROTATE: RotateImage()
+        }
+        return transformations.get(transform_id)
